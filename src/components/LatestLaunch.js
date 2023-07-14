3bdac4c010bf37bbdc12d4380f08c5850ecb839c
Traasko <56584321+Traasko@users.noreply.github.com>
2023-07-14 11:35:37 +0200

    Add lastLunched

--- src/components/LatestLaunch.js
+++ src/components/LatestLaunch.js
@@ -1,156 +1,147 @@
 import React, { useState, useEffect } from 'react';
-import { format } from 'date-fns';
+import Call from '../functions/Call';
 
 function LastLaunchInfo() {
-    const [lastLaunch, setLastLaunch] = useState(null);
+    const [lastLaunched, setLastLaunched] = useState(null);
     const [crewMembers, setCrewMembers] = useState([]);
     const [capsules, setCapsules] = useState([]);
     const [payloads, setPayloads] = useState([]);
     const [launchpad, setLaunchpad] = useState(null);
+    const lastLaunch = Call('https://api.spacexdata.com/v5/launches/latest');
 
-    useEffect(() => {
-        fetch('https://api.spacexdata.com/v5/launches/latest')
-            .then((response) => response.json())
+    // useEffect(() => {
+    //     fetch('https://api.spacexdata.com/v5/launches/latest')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setLastLaunch(data);
+    //             fetchCrewMembers(data.crew);
+    //             fetchCapsules(data.capsules);
+    //             fetchPayloads(data.payloads);
+    //             fetchLaunchpad(data.launchpad);
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error fetching latest launch:', error);
+    //         });
+    // }, []);
 
-            .then((data) => {
-                setLastLaunch(data);
-                fetchCrewMembers(data.crew);
-                fetchCapsules(data.capsules);
-                fetchPayloads(data.payloads);
-                fetchLaunchpad(data.launchpad);
-            })
+    // const fetchCrewMembers = (crewIds) => {
+    //     const requests = crewIds.map((crew) => crew.crew);
 
-            .catch((error) => {
-                console.error('Error fetching latest launch:', error);
-            });
-    }, []);
+    //     Promise.all(
+    //         requests.map((id) =>
+    //             fetch(`https://api.spacexdata.com/v4/crew/${id}`).then(
+    //                 (response) => response.json(),
+    //             ),
+    //         ),
+    //     )
+    //         .then((data) => {
+    //             const crewMembersData = data.map((response) => response);
+    //             setCrewMembers(crewMembersData);
+    //             console.log('members', crewMembersData);
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error fetching crew members:', error);
+    //         });
+    // };
 
-    const fetchCrewMembers = (crewIds) => {
-        const requests = crewIds.map((crew) => crew.crew);
+    // const fetchCapsules = (capsuleIds) => {
+    //     const requests = capsuleIds.map((id) =>
+    //         fetch(`https://api.spacexdata.com/v4/capsules/${id}`).then(
+    //             (response) => response.json(),
+    //         ),
+    //     );
 
-        Promise.all(
-            requests.map((id) =>
-                fetch(`https://api.spacexdata.com/v4/crew/${id}`).then(
-                    (response) => response.json(),
-                ),
-            ),
-        )
+    //     Promise.all(requests)
+    //         .then((data) => {
+    //             setCapsules(data);
+    //             console.log('capsules', data);
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error fetching capsules:', error);
+    //         });
+    // };
 
-            .then((data) => {
-                const crewMembersData = data.map((response) => response);
-                setCrewMembers(crewMembersData);
-                console.log('members', crewMembersData);
-            })
-            .catch((error) => {
-                console.error('Error fetching crew members:', error);
-            });
-    };
+    // const fetchPayloads = (payloadIds) => {
+    //     const requests = payloadIds.map((id) =>
+    //         fetch(`https://api.spacexdata.com/v4/payloads/${id}`).then(
+    //             (response) => response.json(),
+    //         ),
+    //     );
 
-    const fetchCapsules = (capsuleIds) => {
-        const requests = capsuleIds.map((id) =>
-            fetch(`https://api.spacexdata.com/v4/capsules/${id}`).then(
-                (response) => response.json(),
-            ),
-        );
+    //     Promise.all(requests)
+    //         .then((data) => {
+    //             setPayloads(data);
+    //             console.log('capsules', data);
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error fetching payloads:', error);
+    //         });
+    // };
 
-        Promise.all(requests)
-            .then((data) => {
-                setCapsules(data);
-                console.log('capsules', data);
-            })
+    // const fetchLaunchpad = (launchpadId) => {
+    //     fetch(`https://api.spacexdata.com/v4/launchpads/${launchpadId}`)
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setLaunchpad(data);
+    //             console.log('launchpad', data);
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error fetching launchpad:', error);
+    //         });
+    // };
 
-            .catch((error) => {
-                console.error('Error fetching capsules:', error);
-            });
-    };
-
-    const fetchPayloads = (payloadIds) => {
-        const requests = payloadIds.map((id) =>
-            fetch(`https://api.spacexdata.com/v4/payloads/${id}`).then(
-                (response) => response.json(),
-            ),
-        );
-
-        Promise.all(requests)
-
-            .then((data) => {
-                setPayloads(data);
-                console.log('capsules', data);
-            })
-
-            .catch((error) => {
-                console.error('Error fetching payloads:', error);
-            });
-    };
-
-    const fetchLaunchpad = (launchpadId) => {
-        fetch(`https://api.spacexdata.com/v4/launchpads/${launchpadId}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setLaunchpad(data);
-
-                console.log('launchpad', data);
-            })
-
-            .catch((error) => {
-                console.error('Error fetching launchpad:', error);
-            });
-    };
-
-    const formatDate = (dateString) => {
-        const dateObject = new Date(dateString);
-        const formattedDate = format(dateObject, "do 'of' MMMM yyyy");
-        return formattedDate;
-    };
+    // const formatDate = (dateString) => {
+    //     const dateObject = new Date(dateString);
+    //     const formattedDate = format(dateObject, "do 'of' MMMM yyyy");
+    //     return formattedDate;
+    // };
 
     return (
         <div>
-            <h1>Last Launch Information</h1>
-
-            <section>
-                <div>
-                    <h2>Details:</h2>
+            <h1 id="title-last-launch">Last Launch Information</h1>
+            <section id="details">
+                <div className="div-section-details">
+                    <h2 className="h2_last-_lauch">Details</h2>
                     <p>Flight Number {lastLaunch.flight_number}</p>
                     <p>Mission {lastLaunch.name}</p>
-                    <p>{formatDate(lastLaunch.date_local)}</p>
                 </div>
             </section>
-
-            <section>
+            <section id="members">
                 <div>
-                    <h2>Crew Members</h2>
+                    <h2 className="h2_last-_lauch">Crew Members</h2>
                     <div>
                         {crewMembers.map((crew) => (
-                            <div key={crew.id}>
-                                <h3>{crew.name}</h3>
-                                <img src={crew.image}></img>
+                            <div id="members-title-img" key={crew.id}>
+                                <h3 id="name-member">{crew.name}</h3>
+                                <a href={crew.wikipedia} target="blank">
+                                    <img
+                                        src={crew.image}
+                                        className="img-membres-last-lauch"
+                                    ></img>
+                                </a>
                             </div>
                         ))}
                     </div>
                 </div>
             </section>
-
-            <h2>Capsules:</h2>
-            <ul>
+            <div>
                 {capsules.map((capsule) => (
-                    <li key={capsule.id}>{capsule.serial}</li>
+                    <div key={capsule.id}>
+                        <h2 className="h2_last-_lauch">Capsules</h2>
+                        <h3>{capsule.serial}</h3>
+                        <h3>{capsule.type}</h3>
+                    </div>
                 ))}
-            </ul>
-
-            <h2>Payloads:</h2>
-            <ul>
-                {payloads.map((payload) => (
-                    <li key={payload.id}>{payload.name}</li>
-                ))}
-            </ul>
-
-            <h2>Launchpad:</h2>
+            </div>
+            <h2 className="h2_last-_lauch">Launchpad</h2>
             {launchpad && (
                 <div>
-                    <p>Name: {launchpad.name}</p>
+                    <p>{launchpad.full_name}</p>
                     <p>
-                        Location: {launchpad.locality}, {launchpad.region}
+                        {launchpad.locality}, {launchpad.region}
                     </p>
+                    <p className="padding-last-lauch">{launchpad.details}</p>
+                    <img src={launchpad.images.large} id="img-launchpad"></img>
                 </div>
             )}
         </div>
